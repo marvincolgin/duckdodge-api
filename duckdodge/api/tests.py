5777882a0998a126e8ab4203c8545f4ce428f0da
Marvin Colgin <bpadmin@builtbp.com>
2019-10-13 14:32:49 -0700

    TEST: tests for models... boat/race

--- duckdodge/api/tests.py
+++ duckdodge/api/tests.py
@@ -1,3 +1,34 @@
+import datetime
 from django.test import TestCase
+from .models import Boat, Race
 
-# Create your tests here.
+
+class BoatTest(TestCase):
+
+    def setUp(self):
+        Boat.objects.create(
+            boat='Fat Chance', phrf=186, make='J/24'
+        )
+
+    def test_boat(self):
+        boat = Boat.objects.get(boat='Fat Chance')
+        self.assertEqual(boat.phrf, 186)
+
+
+class RaceTest(TestCase):
+
+    def setUp(self):
+        Race.objects.create(
+            racedate=datetime.datetime(2007, 1, 1),
+            start=2,
+            place=3,
+            boat='Fat Chance'
+        )
+
+    def test_race(self):
+        race = Race.objects.get(
+            racedate=datetime.datetime(2007, 1, 1),
+            start=2,
+            place=3
+        )
+        self.assertEqual(race.boat, 'Fat Chance')
