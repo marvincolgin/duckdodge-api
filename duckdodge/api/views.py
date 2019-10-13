2b3fbc06d8e42c889dfdb527570da612da28ef68
Marvin Colgin <bpadmin@builtbp.com>
2019-10-12 21:22:21 -0700

    ADD: CRUD REST API for Boats/Races.

--- duckdodge/api/views.py
+++ duckdodge/api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Boat, Race
+from .serializers import BoatSerializer, RaceSerializer
+# from .permissions import IsCreatorOrReadOnly
 
-# Create your views here.
+class RaceList(generics.ListCreateAPIView):
+    queryset = Race.objects.all()
+    serializer_class = RaceSerializer
+
+class BoatList(generics.ListCreateAPIView):
+    queryset = Boat.objects.all()
+    serializer_class = BoatSerializer
+
+class RaceDetail(generics.RetrieveUpdateDestroyAPIView):
+    # permission_classes = (IsCreatorOrReadOnly, )
+    queryset = Race.objects.all()
+    serializer_class = RaceSerializer
+
+class BoatDetail(generics.RetrieveUpdateDestroyAPIView):
+    # permission_classes = (IsCreatorOrReadOnly, )
+    queryset = Boat.objects.all()
+    serializer_class = BoatSerializer
