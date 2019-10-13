4a424af1e1ae3dfeae3e849b2247ea179b5ba6d8
Marvin Colgin <bpadmin@builtbp.com>
2019-10-12 21:05:37 -0700

    ADD: Model for Duckdoge

--- duckdodge/api/admin.py
+++ duckdodge/api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Boat, Race
 
 # Register your models here.
+admin.site.register(Boat)
+admin.site.register(Race)
--- duckdodge/api/models.py
+++ duckdodge/api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+class Boat(models.Model):
+    id = models.AutoField(primary_key=True)
+    boat = models.CharField(max_length=255)
+    phrf = models.IntegerField(default=None)
+    make = models.CharField(max_length=50, default=None)
+
+    def __str__(self):
+        return f'{self.boat} / {self.make} phrf:{self.phrf}'
+
+class Race(models.Model):
+    racedate = models.DateField()
+    start = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(4)])
+    place = models.PositiveIntegerField()
+    boat = models.CharField(max_length=255)
+
+    class Meta:
+        unique_together = ('racedate', 'start', 'place')
+
+    def __str__(self):
+        return f'{self.racedate} Start:{self.start} Place:{self.place} {self.boat}'
